ae212b8a8ae65d4a249c809f1cdca4c2ad0e2abf
snickbit <nicklowe@snickbit.com>
2022-05-24 13:44:08 -0400

    fix: set reader to only emit once

--- src/helpers.ts
+++ src/helpers.ts
@@ -34,7 +34,7 @@ export async function getFirstLine(pathToFile) {
 	const readable = fs.createReadStream(pathToFile)
 	const reader = readline.createInterface({input: readable})
 	const line = await new Promise((resolve) => {
-		reader.on('line', (line) => {
+		reader.once('line', (line) => {
 			reader.close()
 			resolve(line)
 		})
