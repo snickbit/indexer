5bfc47e63e40fa0db8034a901af3552448997fc7
Snickbit <snickbit@users.noreply.github.com>
2022-06-13 19:56:45 -0400

    style: lint

--- src/generate-indexes.ts
+++ src/generate-indexes.ts
@@ -163,9 +163,9 @@ function resolvePath(source: string, file: string): string {
 	const resolvedIndex = posix.resolve(source)
 	const resolvedFile = posix.resolve(file)
 	let file_path = posix.relative(resolvedIndex, resolvedFile)
-	                     .replace(/^(\.\.)?\/?/, './')
-	                     .replace(/\.[jt]s$/, '')
-	                     .replace(/\/index$/, '')
+		.replace(/^(\.\.)?\/?/, './')
+		.replace(/\.[jt]s$/, '')
+		.replace(/\/index$/, '')
 	if (file_path === '.') {
 		file_path = './index'
 	}
